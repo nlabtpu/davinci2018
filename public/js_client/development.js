7f94b722beb2c5e2eab27f6c7e7227165dc224dc
Sahara62 <cloud_1827_862@yahoo.co.jp>
2018-06-29 20:32:58 +0900

    srcフォルダを追加し，jsxファイルを移動
    srcフォルダ作るのを忘れていた

--- public/js_client/development.js
+++ public/js_client/development.js
@@ -17,7 +17,7 @@ export default {
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        loader: 'babel'
+        loader: 'babel-loader'
       }
     ]
   },
--- public/js_client/webpack.config.js
+++ public/js_client/webpack.config.js
@@ -1,2 +1,26 @@
 require('babel-core/register'); // development.jsでES6を使えるようにする
-module.exports = require('./development');
+module.exports = {
+  mode: 'development',
+  entry: './src/index.jsx',
+
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  plugins: []
+};

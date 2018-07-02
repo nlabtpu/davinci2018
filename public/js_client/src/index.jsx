5acdad86be5caf53e738f6260edaf6bfd486d9c2
Sahara62 <cloud_1827_862@yahoo.co.jp>
2018-07-02 22:27:27 +0900

    ぼたん書き間違えしていたので修正

--- public/js_client/src/index.jsx
+++ public/js_client/src/index.jsx
@@ -15,7 +15,6 @@ class App extends React.Component {
   onClickSend(e){
     if(props.command === 0) return fales;
     console.log('send');
-    socket.emit('message', JSON.stringify(props));
   }
   onClick(e){
     console.log('click');
@@ -26,11 +25,11 @@ class App extends React.Component {
       <div>
         <p>{this.state.value}</p>
         <input type="text" value={this.state.value} onChange={this.onChange.bind(this)}/>
-        <button type="button" id="inputname" onClick={this.onClick.bind(this)}/>
-        <button type="button" id="send" onClickSend={this.onClickSend.bind(this)}/>
-        <button type="button" id="reset" onClick={this.onClick.bind(this)}/>
-        <button type="button" id="go" onClick={this.onClick.bind(this)}/>
-        <button type="button" id="roll" onClick={this.onClick.bind(this)}/>
+        <button type="button" id="inputname" onClick={this.onClick.bind(this)}>決定</button>
+        <button type="button" id="send" onClickSend={this.onClickSend.bind(this)}>送信</button>
+        <button type="button" id="reset" onClick={this.onClick.bind(this)}>リセット</button>
+        <button type="button" id="go" onClick={this.onClick.bind(this)}>Ｇｏ</button>
+        <button type="button" id="roll" onClick={this.onClick.bind(this)}>Ｒｏｌｌ</button>
         <ul id="messageList"></ul>
       </div>
     );
